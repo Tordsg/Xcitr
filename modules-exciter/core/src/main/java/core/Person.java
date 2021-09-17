3ac547c02f27d8e4e0513b98b0e21fd9a46af2c0
theasalvesen <theassa@stud.ntnu.no>
2021-09-17 13:46:17 +0200

    Made getters and setters

--- modules-exciter/core/src/main/java/core/Person.java
+++ modules-exciter/core/src/main/java/core/Person.java
@@ -9,7 +9,60 @@ public class Person{
     private List<String> interests = new ArrayList<>();
     private String bio;
 
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public void setInterests(List<String> interests){
+        this.interests = interests;
+    }
+
+    public void setBio(String bio){
+        this.bio = bio;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    public List<String> getInterests(){
+        return interests;
+    }
+
+    public String getBio(){
+        return Bio;
+    }
+
+    public void addInterests(String newInterest){
+        interests.add(newInterest);
+    }
     
+    public void removeInterest(String removeInterest){
+        for (int i = 0; i < interest.size(); i++){
+            if(interests.get(i).equals(removeInterest)){
+                int index = i;
+
+            }
+        }
+        interest.remove(index);
+    }
+
+
+public static void main(String[] args){
+    Person Thea = new Person();
+    Thea.setName("Thea");
+    System.out.println(Thea.getName());
+}
+
+
     
     
     }
