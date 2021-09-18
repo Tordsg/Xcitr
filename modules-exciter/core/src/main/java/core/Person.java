0b111fede0339a8a1acd0b400225e7aa6f2d215d
theasalvesen <theassa@stud.ntnu.no>
2021-09-18 15:38:01 +0200

    made the basic info in person class

--- modules-exciter/core/src/main/java/core/Person.java
+++ modules-exciter/core/src/main/java/core/Person.java
@@ -1,68 +1,60 @@
+package core;
 
+import java.util.List;
+import java.util.ArrayList;
 
-
-public class Person{
-
+public class Person {
 
     private String name;
     private int age;
     private List<String> interests = new ArrayList<>();
     private String bio;
 
+    public String getName() {
+        return this.name;
+    }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setAge(int age){
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 
-    public void setInterests(List<String> interests){
+    public List<String> getInterests() {
+        return this.interests;
+    }
+
+    public void setInterests(List<String> interests) {
         this.interests = interests;
     }
 
-    public void setBio(String bio){
+    public String getBio() {
+        return this.bio;
+    }
+
+    public void setBio(String bio) {
         this.bio = bio;
     }
 
-    public String getName(){
-        return name;
+    public void addInterest(String interest){
+        interests.add(interest);
     }
 
-    public int getAge(){
-        return age;
-    }
-    public List<String> getInterests(){
-        return interests;
-    }
-
-    public String getBio(){
-        return Bio;
-    }
-
-    public void addInterests(String newInterest){
-        interests.add(newInterest);
-    }
-    
-    public void removeInterest(String removeInterest){
-        for (int i = 0; i < interest.size(); i++){
-            if(interests.get(i).equals(removeInterest)){
-                int index = i;
-
+    public void removeInterest(String interest){
+        int index = 0;
+        for (int i = 0; i < interests.size(); i++) {
+            if(interests.get(i).equals(interest)){
+                index = i;
             }
+            
         }
-        interest.remove(index);
+        interests.remove(index);
     }
-
-
-public static void main(String[] args){
-    Person Thea = new Person();
-    Thea.setName("Thea");
-    System.out.println(Thea.getName());
+    
 }
-
-
-    
-    
-    }
