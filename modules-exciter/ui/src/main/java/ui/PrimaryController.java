bad83e779498688a23fef9d243ecff03c6eb2fd6
theasalvesen <theassa@stud.ntnu.no>
2021-09-20 15:13:45 +0200

    connected the controller to fxml primary

--- modules-exciter/ui/src/main/java/ui/PrimaryController.java
+++ modules-exciter/ui/src/main/java/ui/PrimaryController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
 import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 
 public class PrimaryController {
 
@@ -19,4 +20,42 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+    @FXML
+    private Button Like1;
+
+    @FXML
+    private ImageView Image1;
+
+    @FXML
+    private Label Name1;
+
+    @FXML
+    private Label Bio1;
+
+    @FXML
+    private Button Like2;
+
+    @FXML
+    private ImageView Image2;
+
+    @FXML
+    private Label Name2;
+
+    @FXML
+    private Label Bio2;
+
+    @FXML
+    private Button Profile;
+
+    @FXML
+    void onLike1(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onLike2(ActionEvent event) {
+
+    }
+
+
 }
