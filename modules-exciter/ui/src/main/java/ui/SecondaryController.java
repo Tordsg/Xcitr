88fa82c445d2edc043fd6e8ebc0f078e3beeabac
theasalvesen <theassa@stud.ntnu.no>
2021-09-20 15:38:36 +0200

    Connected the controller to FXML file

--- modules-exciter/ui/src/main/java/ui/SecondaryController.java
+++ modules-exciter/ui/src/main/java/ui/SecondaryController.java
@@ -3,7 +3,14 @@ package ui;
 import core.Exciter;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class SecondaryController {
 
@@ -11,4 +18,24 @@ public class SecondaryController {
     private void switchToPrimary() throws IOException {
         App.setRoot("primary");
     }
+
+@FXML
+private Button back;
+
+@FXML
+private ImageView picture;
+
+@FXML
+private TextArea Bio;
+
+
+@FXML
+private TextField mail;
+
+@FXML
+void updateSite(ActionEvent event){
+    
+}
+
+
 }
