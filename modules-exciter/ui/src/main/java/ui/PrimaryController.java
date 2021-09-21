1ffd51057dcc0a400c74afa7fd1a07ee916a7d1a
theasalvesen <theassa@stud.ntnu.no>
2021-09-21 11:03:26 +0200

    removed unused imports

--- modules-exciter/ui/src/main/java/ui/PrimaryController.java
+++ modules-exciter/ui/src/main/java/ui/PrimaryController.java
@@ -3,16 +3,9 @@ package ui;
 import core.Exciter;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.function.BinaryOperator;
-import java.util.function.UnaryOperator;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.control.Labeled;
-import javafx.scene.control.ListView;
-import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
 
 public class PrimaryController {
--- modules-exciter/ui/src/main/java/ui/SecondaryController.java
+++ modules-exciter/ui/src/main/java/ui/SecondaryController.java
@@ -7,10 +7,8 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
 
 public class SecondaryController {
     private Exciter excite;
