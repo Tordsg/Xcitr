fd6c68d6f27cccd65032c336c4bc18c18a42798f
theasalvesen <theassa@stud.ntnu.no>
2021-09-21 11:35:48 +0200

    accessed the methods

--- modules-exciter/ui/src/main/java/ui/PrimaryController.java
+++ modules-exciter/ui/src/main/java/ui/PrimaryController.java
@@ -40,34 +40,33 @@ public class PrimaryController {
 
     @FXML
     private Label Age2;
-    @FXML
-    private Button Profile;
+
 
     @FXML
     void onLike1(ActionEvent event) {
-       /*excite.pressedLikeFirst();
+       excite.pressedLikeFirst();
        setUsers();
-       */
+       
 
     }
 
     @FXML
     void onLike2(ActionEvent event) {
-        /*excite.pressedLikeSecond();
+        excite.pressedLikeSecond();
         setUsers();
-        */
+        
 
 
     }
 
     public void setUsers(){
-        /*ArrayList<User> displayUsers = excite.getNextUsers();
+        ArrayList<User> displayUsers = excite.getNextUsers();
         User user1 = displayUsers.get(0);
         User user2 = displayUsers.get(1);
         Name1.setText(user1.getName());
         Age1.setText(user1.getAge());
         Name2.setText(user2.getName());
-        Age2.setText(user2.getAge());*/
+        Age2.setText(user2.getAge());
 
     }
 
