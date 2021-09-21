cb589e3d37dd8e384c894576b950c8e073f5dc61
theasalvesen <theassa@stud.ntnu.no>
2021-09-21 10:55:31 +0200

    made connection between Exciter and FXML information

--- modules-exciter/ui/src/main/java/ui/PrimaryController.java
+++ modules-exciter/ui/src/main/java/ui/PrimaryController.java
@@ -17,6 +17,9 @@ import javafx.scene.control.Button;
 
 public class PrimaryController {
 
+    private Exciter excite;
+
+
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
@@ -25,19 +28,16 @@ public class PrimaryController {
     private Button Like1;
 
     @FXML
-    private ImageView Image1;
-
-    @FXML
     private Label Name1;
 
     @FXML
     private Label Bio1;
 
     @FXML
-    private Button Like2;
+    private Label Age1;
 
     @FXML
-    private ImageView Image2;
+    private Button Like2;
 
     @FXML
     private Label Name2;
@@ -46,15 +46,33 @@ public class PrimaryController {
     private Label Bio2;
 
     @FXML
+    private Label Age2;
+    @FXML
     private Button Profile;
 
     @FXML
     void onLike1(ActionEvent event) {
+       /*excite.pressedLikeFirst();
+       */
 
     }
 
     @FXML
     void onLike2(ActionEvent event) {
+        /*excite.pressedLikeSecond();
+        */
+
+
+    }
+
+    public void setUsers(){
+        /*ArrayList<User> displayUsers = excite.getNextUsers();
+        User user1 = displayUsers.get(0);
+        User user2 = displayUsers.get(1);
+        Name1.setText(user1.getName());
+        Age1.setText(user1.getAge());
+        Name2.setText(user2.getName());
+        Age2.setText(user2.getAge());*/
 
     }
 
