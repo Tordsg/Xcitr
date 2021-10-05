a444237c76663a180cc4f3e842e0ed8d60df4d7a
Henrik <henrik@stud.ntnu.no>
2021-10-05 11:58:56 +0200

    JSON expanded to read more information

--- modules-exciter/json/src/test/java/json/JsonTest.java
+++ modules-exciter/json/src/test/java/json/JsonTest.java
@@ -4,27 +4,43 @@ package json;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 
-import core.User;
+import core.*;
 
 public class JsonTest {
 
     FileHandler fileHandler = new FileHandler();
     private User user;
+    private Exciter exciter;
 
-    @Before
+    @BeforeEach
     public void setUp() {
+        exciter = new Exciter();
         fileHandler.createFile();
+        user = new User("Ola Nordmann", 26, "Fiskesprett på søndager","ola@mail");
+        exciter.setCurrentUser(user);
+
     }
 
     @Test
     public void readFromFile(){
-        user = new User("Ola Nordmann", 26, "Fiskesprett på søndager","ola@mail");
         fileHandler.saveUser(user);
         User userReadFromFile = fileHandler.readUser();
         Assertions.assertEquals("Ola Nordmann", userReadFromFile.getName());
         Assertions.assertEquals(26, userReadFromFile.getAge());
         Assertions.assertEquals("ola@mail", userReadFromFile.getEmail());
+    }
+
+    @Test
+    public void readMatches(){
+        User onScreenUser1 = exciter.getOnScreenUser1();
+        for (int i = 0; i < 3; i++) {
+            exciter.pressedLikeFirst();
+        }
+        fileHandler.saveUser(user);
+        User userReadFromFile = fileHandler.readUser();
+        Assertions.assertTrue(exciter.pressedLikeFirst());
     }
 
 }
