f4596cb14b47be43589b8c1ede248204f802aef6
Selma <selma.gudmundsen@gmail.com>
2021-10-13 15:42:31 +0200

    checks for no repetition of email adress

--- modules-exciter/ui/src/main/java/ui/SignUpController.java
+++ modules-exciter/ui/src/main/java/ui/SignUpController.java
@@ -4,12 +4,74 @@ import core.*;
 import json.*;
 
 import java.io.IOException;
+
+import org.dom4j.Text;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class SignUpController {
 
     @FXML
-    public void switchToLogIn() throws IOException {
+    Button createAccount;
+    
+    @FXML
+    TextField name;
+
+    @FXML
+    TextField age;
+
+    @FXML
+    TextField emailSignup;
+
+    @FXML
+    PasswordField passwordSignup;
+
+    @FXML
+    Text errorMessage;
+
+    FileHandler fileHandler;
+    User user;
+
+    @FXML
+    void initialize() {
+        name.clear();
+        age.clear();
+        emailSignup.clear();
+        passwordSignup.clear();
+        errorMessage.setVisible(false);
+
+    }
+
+    @FXML
+    void onSwitchToLogIn() throws IOException {
         App.setRoot("login");
     }
+
+    @FXML
+    void handleCreateAccount() throws IOException {
+        String nameReg = name.getText();
+        String ageReg = age.getText();
+        String emailReg = emailSignup.getText();
+        String passwordReg = passwordSignup.getText();
+        
+        for (User user : fileHandler.readUsers()) {
+            if (emailReg.equals(user.getEmail())) { 
+                errorMessage.setVisible(true);
+                emailSignup.clear();
+                passwordSignup.clear();
+            }
+        }
+        this.user = new User(nameReg, Integer.parseInt(ageReg), emailReg);
+        user.setPassword(passwordReg);
+
+        fileHandler.saveUser(user);
+       
+        App.setRoot("primary");
+
+    }
+
 }
