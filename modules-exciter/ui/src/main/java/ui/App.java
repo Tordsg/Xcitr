8f202fbcbeeca4fde5dce1d71e14c03ce4859e88
tordsg <tordsg@stud.ntnu.no>
2021-10-15 12:20:33 +0200

    added app logo, name and non resizable

--- modules-exciter/ui/src/main/java/ui/App.java
+++ modules-exciter/ui/src/main/java/ui/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,6 +22,9 @@ public class App extends Application {
         App.stage = stage;
         scene = new Scene(loadFXML("login"));
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("citr");
+        stage.getIcons().add(new Image(App.class.getResourceAsStream("Images/logo.png")));
         stage.show();
     }
 
