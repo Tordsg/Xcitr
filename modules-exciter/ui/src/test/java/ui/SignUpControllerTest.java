f8d6c38be2cc382dcd92f9db85e739ebfff4d07b
Henrik <henrik@stud.ntnu.no>
2021-10-19 10:14:33 +0200

    one working signuptest

--- modules-exciter/ui/src/test/java/ui/SignUpControllerTest.java
+++ modules-exciter/ui/src/test/java/ui/SignUpControllerTest.java
@@ -1,13 +1,12 @@
 package ui;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -21,52 +20,60 @@ import core.User;
 public class SignUpControllerTest extends ApplicationTest {
 
   private SignUpController controller = new SignUpController();
-  private Exciter excite = new Exciter();
-  
+  private App app = new App();
+  private Exciter excite = App.exciter;
+
   @Override
   public void start(Stage stage) throws Exception {
-    FXMLLoader loader = new FXMLLoader(getClass().getResource("signup.fxml"));
-    Parent root = loader.load();
-    controller = loader.getController();
-    stage.setScene(new Scene(root));
-    stage.show();
+    app.start(stage);
   }
 
   public SignUpController getController() {
     return controller;
   }
 
+  @BeforeEach
+  public void setup() {
+    app = new App();
+    clickOn("#fromLoginToSignup");
+  }
+
+  @AfterEach
+  public void tearDown() throws Exception {
+    app.stop();
+  }
+
   @ParameterizedTest
   @MethodSource
-  public void testController(String string1, boolean excpected) {
-    checkResult(string1, excpected);
+  public void testController(boolean excpected) {
+    checkResult(excpected);
 
   }
 
   private static Stream<Arguments> testController() {
-    return Stream.of(Arguments.of(null, true));
+    return Stream.of(Arguments.of(true));
   }
 
   // This is first method, where all you do is pass what's comes
   // from the test method to the checkResult method
   // Why you keep this one is to split up the first parameter if it's a list for
   // example
-  @ParameterizedTest
+  /*@ParameterizedTest
   @MethodSource
   public void test1equal1(int one, int two) {
     checkResult(one, two);
-  }
+  }*/
 
   // This is second method, where you keep all the values
   // you want to test. i.e. you can have a list of values
   // like name, age, etc.
-  private static Stream<Arguments> test1equal1() {
+  /*private static Stream<Arguments> test1equal1() {
     return Stream.of(Arguments.of(1, 1));
-  }
+  }*/
 
   // third method, where you do the assertions
   // and you actually call the click, lookup, whatever methods
-  private void checkResult(String string1, boolean excpected) {
+  private void checkResult(boolean excpected) {
     //How to fill textboxes
     TextField name = lookup("#name").query();
     clickOn(name);
@@ -85,27 +92,28 @@ public class SignUpControllerTest extends ApplicationTest {
     TextField password = lookup("#passwordSignup").query();
     clickOn(password);
     write("123");
-    //Simple click. It refers to fxml id
+
+
     clickOn("#createAccount");
     //Assertions placeholder
-    checkIfContainsUser(currentUser);
-    Assertions.assertNull(string1);
+    //checkIfContainsUser(currentUser);
+    Assertions.assertTrue(excite.getCurrentUser().getEmail().equals(currentUser.getEmail()));
   }
 
   private void checkResult(int one, int two) {
     Assertions.assertEquals(one, two);
   }
 
-  private void checkIfContainsUser(User user){
+  /*private void checkIfContainsUser(User user){
     int number = 0;
     if(excite.getAllUsers().contains(user)){
       number = 1;
     }
     test1equal1(1, number);
-   
-}
 
- 
+}*/
+
+
 
   // TODO: Add more tests
 
