c06eb17fe007140b576d610f8f56420a78a56334
Henrik Lovhaug <henrik.lovhaug@gmail.com>
2021-11-02 19:25:23 +0100

    Formated according to checkstyle

--- modules-exciter/user/src/main/java/user/UserDeserializer.java
+++ modules-exciter/user/src/main/java/user/UserDeserializer.java
@@ -12,63 +12,57 @@ import com.fasterxml.jackson.databind.node.IntNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.node.TextNode;
 
-public class UserDeserializer extends StdDeserializer<User>{
+public class UserDeserializer extends StdDeserializer<User> {
 
-    public UserDeserializer() {
-        this(null);
+  public UserDeserializer() {
+    this(null);
+  }
+
+  public UserDeserializer(Class<?> vc) {
+    super(vc);
+  }
+
+  @Override
+  public User deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+    JsonNode node = jp.getCodec().readTree(jp);
+    return deserialize(node);
+  }
+
+  public User deserialize(JsonNode node) {
+    if (node instanceof ObjectNode objectNode) {
+      User user = new User();
+      JsonNode nameNode = objectNode.get("name");
+      if (nameNode instanceof TextNode) {
+        user.setName(nameNode.asText());
+      }
+      JsonNode ageNode = objectNode.get("age");
+      if (ageNode instanceof IntNode) {
+        user.setAge(ageNode.asInt());
+      }
+      JsonNode emailNode = objectNode.get("email");
+      if (emailNode instanceof TextNode) {
+        user.setEmail(emailNode.asText());
+      }
+      ArrayNode matchNode = (ArrayNode) objectNode.get("matches");
+      if (matchNode instanceof ArrayNode) {
+        for (JsonNode string : matchNode) {
+          if (string instanceof TextNode) {
+            user.addMatch(string.asText());
+          }
+        }
+      }
+      JsonNode userInfoNode = objectNode.get("userInformation");
+      if (userInfoNode instanceof TextNode) {
+        user.setUserInformation(userInfoNode.asText());
+      }
+      JsonNode passwordNode = objectNode.get("password");
+      if (passwordNode instanceof TextNode) {
+        user.setPassword(passwordNode.asText());
+      }
+
+      return user;
     }
-
-    public UserDeserializer(Class<?> vc) {
-        super(vc);
-    }
-
-    @Override
-    public User deserialize(JsonParser jp, DeserializationContext ctxt)
-      throws IOException, JsonProcessingException {
-        JsonNode node = jp.getCodec().readTree(jp);
-        return deserialize(node);
-    }
-
-    public User deserialize(JsonNode node) {
-        if(node instanceof ObjectNode objectNode){
-            User user = new User();
-            JsonNode nameNode = objectNode.get("name");
-            if (nameNode instanceof TextNode) {
-                user.setName(nameNode.asText());
-              }
-              JsonNode ageNode = objectNode.get("age");
-              if (ageNode instanceof IntNode) {
-                user.setAge(ageNode.asInt());
-              }
-              JsonNode emailNode = objectNode.get("email");
-              if (emailNode instanceof TextNode) {
-                user.setEmail(emailNode.asText());
-              }
-              ArrayNode matchNode = (ArrayNode) objectNode.get("matches");
-              if (matchNode instanceof ArrayNode) {
-                for (JsonNode string : matchNode) {
-                  if (string instanceof TextNode) {
-                    user.addMatch(string.asText());
-                  }
-                }
-              }
-              JsonNode userInfoNode = objectNode.get("userInformation");
-              if (userInfoNode instanceof TextNode) {
-                user.setUserInformation(userInfoNode.asText());
-              }
-              JsonNode passwordNode = objectNode.get("password");
-              if (passwordNode instanceof TextNode) {
-                user.setPassword(passwordNode.asText());
-              }
-
-
-
-              return user;
-            }
-            return null;
-    }
+    return null;
+  }
 
 }
-
-
-
