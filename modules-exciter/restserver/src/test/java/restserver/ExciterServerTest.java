2ee181ae4d5c396946e59a925ef50b0aa404f5de
Henrik Lovhaug <henrik.lovhaug@gmail.com>
2021-11-03 13:08:09 +0100

    Removed comment

--- modules-exciter/restserver/src/test/java/restserver/ExciterServerTest.java
+++ modules-exciter/restserver/src/test/java/restserver/ExciterServerTest.java
@@ -53,7 +53,6 @@ public class ExciterServerTest {
             Assertions.assertEquals(200, response.code());
 
         } catch (Exception e) {
-            // TODO: handle exception
             e.printStackTrace();
         }
     }
