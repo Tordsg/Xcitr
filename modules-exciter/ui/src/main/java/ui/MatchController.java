8c467b8d9308592be620459435b6e85b0fd63b95
tordsg <tordsg@stud.ntnu.no>
2021-11-08 14:54:58 +0100

    match site with partially wokring messages

--- modules-exciter/ui/src/main/java/ui/MatchController.java
+++ modules-exciter/ui/src/main/java/ui/MatchController.java
@@ -15,13 +15,18 @@ import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.effect.Lighting;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
@@ -34,13 +39,13 @@ import javafx.scene.text.Text;
 public class MatchController implements Initializable {
 
   @FXML
-  HBox hBox;
-
+  VBox matchBox, textBox;
   @FXML
-  Group button;
-
+  TextField textInput;
   @FXML
-  AnchorPane anchorPane;
+  Group backButton, sendButton;
+  @FXML
+  AnchorPane anchorPane, profilePane;
 
   protected final static ImageController imageController = PrimaryController.imageController;
   private ClientHandler clientHandler = new ClientHandler();
@@ -59,25 +64,91 @@ public class MatchController implements Initializable {
     try {
       matches = clientHandler.getMatches(user);
     } catch (ServerException e) {
-      //TODO add message
     }
-    hoverButton(button);
+    hoverButton(backButton);
+    hoverButton(sendButton);
+    hoverButton(profilePane);
+    HBox one = createMessage("halllllllllaaaaaa heiii", false);
+    HBox two = createMessage("a", true);
+    HBox three = createMessage("Hei jeg synes du virker som en veldig artig person. Noen ganger tenker jeg at du ikke tenker like mye på andre som du egentlig burde ha gjort.", true);
+    textBox.getChildren().add(one);
+    textBox.getChildren().add(two);
+    textBox.getChildren().add(three);
     if (matches != null && !matches.isEmpty()) {
-      matches.forEach(e -> hBox.getChildren().add(createCard(e)));
+      matches.forEach(e -> matchBox.getChildren().add(createMatchCard(e)));
+      matchBox.getChildren().forEach(e -> hoverButton(e));
     }
-    if (hBox.getChildren().size() == 0) {
+    if (matchBox.getChildren().size() == 0) {
       Label label = new Label();
       label.setText("You have not matches, yet.");
       label.setFont(new Font(30));
       label.setAlignment(Pos.CENTER);
-      label.setLayoutX(140);
-      label.setLayoutY(210);
+      label.setLayoutX(50);
+      label.setLayoutY(100);
       anchorPane.getChildren().add(label);
     } else {
-      dragMatches();
+      matchBox.setOnScroll(new EventHandler<ScrollEvent>() {
+        @Override
+        public void handle(ScrollEvent event) {
+          matchBox.setLayoutY(matchBox.getLayoutY() + event.getDeltaY()/2);
+        }
+      });
+      textBox.setOnScroll(new EventHandler<ScrollEvent>() {
+        @Override
+        public void handle(ScrollEvent event) {
+          if(textBox.getLayoutY() + textBox.getHeight() + event.getDeltaY()/2<393){
+            textBox.setLayoutY(393-textBox.getHeight());
+          } else if(textBox.getLayoutY() + event.getDeltaY()/2>63){
+          }
+            else {
+          textBox.setLayoutY(textBox.getLayoutY() + event.getDeltaY()/2);
+          }
+        }
+      });
     }
   }
-
+  @FXML
+  public void sendMessage(){
+    if(textInput.getText().equals(""))return;
+    HBox hBox = createMessage(textInput.getText(),true);
+    textBox.getChildren().add(hBox);
+    textInput.clear();
+    if(textBox.getLayoutY() + textBox.getHeight()<393){
+      textBox.setLayoutY(393-textBox.getHeight());
+    }
+    
+  }
+  private HBox createMessage(String string, Boolean isCurrentUser){
+    HBox hBox = new HBox();
+    Group group = new Group();
+    Rectangle rectangle = new Rectangle();
+    Text text = new Text();
+    group.getChildren().add(rectangle);
+    group.getChildren().add(text);
+    rectangle.setFill(Color.rgb(220, 220, 220));
+    text.setText(string);
+    if(text.getLayoutBounds().getWidth()>150){
+      text.setWrappingWidth(150);
+    }
+    rectangle.setWidth(text.getLayoutBounds().getWidth()+20);
+    rectangle.setHeight(text.getLayoutBounds().getHeight()+20);
+    if(rectangle.getHeight()<45){
+      rectangle.setArcHeight(25);
+      rectangle.setArcWidth(25);
+    } else {
+      rectangle.setArcHeight(40);
+      rectangle.setArcWidth(40);
+    }
+    text.setLayoutY(rectangle.getHeight()/2-text.getLayoutBounds().getHeight()/2+12);
+    text.setLayoutX(10);
+    hBox.getChildren().add(group);
+    if(isCurrentUser){
+      rectangle.setFill(Color.rgb(0, 190, 255));
+      text.setFill(Color.WHITE);
+      hBox.setAlignment(Pos.TOP_RIGHT);
+    }
+    return hBox;
+  }
   private void hoverButton(Node n) {
     n.setOnMouseEntered(e -> {
       n.setEffect(new Lighting());
@@ -87,111 +158,37 @@ public class MatchController implements Initializable {
     });
   }
 
-  protected static Pane createCard(User user) {
-    Pane pane = new Pane();
-    pane.setPrefHeight(338);
-    pane.setPrefWidth(245);
-    Rectangle rect = new Rectangle();
-    pane.getChildren().add(rect);
-    rect.setArcHeight(45);
-    rect.setArcWidth(45);
-    rect.setHeight(338);
-    rect.setWidth(225);
-    rect.setStrokeWidth(2);
-    rect.setStroke(Color.BLACK);
-    rect.setStrokeType(StrokeType.INSIDE);
-    rect.setFill(imageController.getImage(user));
-    Pane pane1 = new Pane();
-    pane.getChildren().add(pane1);
-    pane1.setCacheShape(false);
-    pane1.setLayoutY(217);
-    pane1.setPrefHeight(121);
-    pane1.setMinHeight(121);
-    pane1.setPrefWidth(225);
-    pane1.setStyle("""
-        -fx-background-color: rgba(255, 255, 255, .4);
-        -fx-background-radius: 0 0 22 22; -fx-border-radius: 0 0 21 21;
-        -fx-border-width: 0 2 2 2; -fx-border-color: black;
-        """);
-    Label age = new Label();
-    pane1.getChildren().add(age);
-    age.setAlignment(Pos.CENTER);
-    age.setContentDisplay(ContentDisplay.CENTER);
-    age.setLayoutX(170);
-    age.setLayoutY(-3);
-    age.setPrefHeight(45);
-    age.setPrefWidth(58);
-    age.setText(Integer.toString(user.getAge()));
-    age.setFont(new Font(30));
-    Label name = new Label();
-    pane1.getChildren().add(name);
-    name.setLayoutX(5);
-    name.setLayoutY(-3);
-    name.setPrefHeight(17);
-    name.setPrefWidth(160);
-    name.setText(user.getName());
-    name.setFont(new Font(30));
-    name.setOpacity(1);
-    Label email = new Label();
-    pane1.getChildren().add(email);
-    email.setOpacity(1);
-    email.setLayoutX(0);
-    email.setLayoutY(100);
-    email.setPrefHeight(12);
-    email.setPrefWidth(225);
-    email.setText(user.getEmail());
-    email.setFont(new Font(12));
-    email.setAlignment(Pos.CENTER);
-    Line line = new Line();
-    pane1.getChildren().add(line);
-    line.setOpacity(1);
-    line.setStartX(50);
-    line.setEndX(175);
-    line.setLayoutX(0);
-    line.setLayoutY(97);
+  protected static Group createMatchCard(User user) {
+    Group group = new Group();
+    Rectangle rectangle = new Rectangle();
+    Circle circle = new Circle();
     Text text = new Text();
-    pane1.getChildren().add(text);
-    text.setOpacity(1);
-    text.setLayoutX(6);
-    text.setLayoutY(50);
-    text.setWrappingWidth(215);
-    text.setText(user.getUserInformation());
-    DropShadow effect = new DropShadow();
-    effect.setOffsetX(2);
-    effect.setOffsetY(2);
-    effect.setColor(new Color(0, 0, 0, 0.5));
-    pane.setEffect(effect);
-    return pane;
+    Label label = new Label();
+    group.getChildren().add(rectangle);
+    group.getChildren().add(circle);
+    group.getChildren().add(text);
+    group.getChildren().add(label);
+    rectangle.setWidth(296);
+    rectangle.setHeight(70);
+    rectangle.setArcHeight(70);
+    rectangle.setArcWidth(70);
+    rectangle.setFill(javafx.scene.paint.Color.WHITE);
+    DropShadow dropShadow = new DropShadow();
+    dropShadow.setOffsetX(5);
+    dropShadow.setOffsetY(5);
+    dropShadow.setColor(Color.rgb(0, 0, 0, 0.5));
+    circle.setEffect(dropShadow);
+    circle.setRadius(24);
+    circle.setLayoutX(38);
+    circle.setLayoutY(34);
+    text.setLayoutX(73);
+    text.setLayoutY(32);
+    text.setText(user.getName());
+    text.setFont(Font.font("System", 19));
+    label.setLayoutX(73);
+    label.setLayoutY(43);
+    label.setPrefWidth(200);
+    label.setMaxWidth(label.getPrefWidth());
+    return group;
   }
-
-  double x;
-  double dX;
-  double lastX;
-
-  private void dragMatches() {
-    hBox.setOnMousePressed(new EventHandler<MouseEvent>() {
-      @Override
-      public void handle(MouseEvent event) {
-        lastX = event.getSceneX();
-
-      }
-    });
-    hBox.setOnMouseDragged(new EventHandler<MouseEvent>() {
-      @Override
-      public void handle(MouseEvent event) {
-        x = event.getSceneX();
-        dX = x - lastX;
-        lastX = x;
-        double hBoxPosition = dX + hBox.getLayoutX();
-        if (hBoxPosition > 20 && dX > 0) {
-          hBox.setLayoutX(hBox.getLayoutX());
-        } else if (hBoxPosition + hBox.getWidth() < 640 && dX < 0) {
-          hBox.setLayoutX(hBox.getLayoutX());
-        } else {
-          hBox.setLayoutX(hBoxPosition);
-        }
-      }
-    });
-  }
-
 }
