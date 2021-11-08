f976dd3a4b97fccfed2129c4441aac645cb3bb96
Henrik Lovhaug <henrik.lovhaug@gmail.com>
2021-11-08 19:33:40 +0100

    Now possible to chat with different people

--- modules-exciter/ui/src/main/java/ui/MatchController.java
+++ modules-exciter/ui/src/main/java/ui/MatchController.java
@@ -54,6 +54,8 @@ public class MatchController implements Initializable {
   @FXML
   Circle chatPic;
 
+  private int chatId;
+
   protected final static ImageController imageController = PrimaryController.imageController;
   private ClientHandler clientHandler = new ClientHandler();
   private User user = App.getUser();
@@ -75,18 +77,23 @@ public class MatchController implements Initializable {
     hoverButton(backButton);
     hoverButton(sendButton);
     hoverButton(chatPic);
-    if(!matches.isEmpty()){
+    if (!matches.isEmpty()) {
       fillChat(user, matches.get(0));
     }
     // HBox one = createMessage("halllllllllaaaaaa heiii", false);
     // HBox two = createMessage("a", true);
-    // HBox three = createMessage("Hei jeg synes du virker som en veldig artig person. Noen ganger tenker jeg at du ikke tenker like mye på andre som du egentlig burde ha gjort.", true);
+    // HBox three = createMessage("Hei jeg synes du virker som en veldig artig
+    // person. Noen ganger tenker jeg at du ikke tenker like mye på andre som du
+    // egentlig burde ha gjort.", true);
     // textBox.getChildren().add(one);
     // textBox.getChildren().add(two);
     // textBox.getChildren().add(three);
     if (matches != null && !matches.isEmpty()) {
       matches.forEach(e -> matchBox.getChildren().add(createMatchCard(e)));
       matchBox.getChildren().forEach(e -> hoverButton(e));
+      for (int i = 0; i < matchBox.getChildren().size(); i++) {
+        clickButton(matchBox.getChildren().get(i), matches.get(i), i);
+      }
     }
     if (matchBox.getChildren().size() == 0) {
       Label label = new Label();
@@ -100,44 +107,48 @@ public class MatchController implements Initializable {
       matchBox.setOnScroll(new EventHandler<ScrollEvent>() {
         @Override
         public void handle(ScrollEvent event) {
-          if(matchBox.getLayoutY()+event.getDeltaY()/2<=62){
-          matchBox.setLayoutY(matchBox.getLayoutY() + event.getDeltaY()/2);
-        } if(matchBox.getLayoutY() + matchBox.getHeight()+ event.getDeltaY()/2 >=420){
-          matchBox.setLayoutY(matchBox.getLayoutY() + event.getDeltaY()/2);
+          if (matchBox.getLayoutY() + event.getDeltaY() / 2 <= 62) {
+            matchBox.setLayoutY(matchBox.getLayoutY() + event.getDeltaY() / 2);
+          }
+          if (matchBox.getLayoutY() + matchBox.getHeight() + event.getDeltaY() / 2 >= 420) {
+            matchBox.setLayoutY(matchBox.getLayoutY() + event.getDeltaY() / 2);
+          }
         }
-        }});
+      });
       textBox.setOnScroll(new EventHandler<ScrollEvent>() {
         @Override
         public void handle(ScrollEvent event) {
-          if(textBox.getLayoutY() + textBox.getHeight() + event.getDeltaY()/2<393){
-            textBox.setLayoutY(393-textBox.getHeight());
-          } else if(textBox.getLayoutY() + event.getDeltaY()/2>63){
-          }
-            else {
-          textBox.setLayoutY(textBox.getLayoutY() + event.getDeltaY()/2);
+          if (textBox.getLayoutY() + textBox.getHeight() + event.getDeltaY() / 2 < 393) {
+            textBox.setLayoutY(393 - textBox.getHeight());
+          } else if (textBox.getLayoutY() + event.getDeltaY() / 2 > 63) {
+          } else {
+            textBox.setLayoutY(textBox.getLayoutY() + event.getDeltaY() / 2);
           }
         }
       });
     }
   }
+
   @FXML
-  public void sendMessage(){
-    if(textInput.getText().equals(""))return;
-    HBox hBox = createMessage(textInput.getText(),true);
+  public void sendMessage() {
+    if (textInput.getText().equals(""))
+      return;
+    HBox hBox = createMessage(textInput.getText(), true);
     try {
-      clientHandler.sendMessage(user, matches.get(0), textInput.getText());
+      clientHandler.sendMessage(user, matches.get(chatId), textInput.getText());
     } catch (ServerException e) {
-      //TODO add error message to screen
+      // TODO add error message to screen
       e.printStackTrace();
     }
     textBox.getChildren().add(hBox);
     textInput.clear();
-    if(textBox.getLayoutY() + textBox.getHeight()<393){
-      textBox.setLayoutY(393-textBox.getHeight());
+    if (textBox.getLayoutY() + textBox.getHeight() < 393) {
+      textBox.setLayoutY(393 - textBox.getHeight());
     }
 
   }
-  private HBox createMessage(String string, Boolean isCurrentUser){
+
+  private HBox createMessage(String string, Boolean isCurrentUser) {
     HBox hBox = new HBox();
     Group group = new Group();
     Rectangle rectangle = new Rectangle();
@@ -146,28 +157,29 @@ public class MatchController implements Initializable {
     group.getChildren().add(text);
     rectangle.setFill(Color.rgb(220, 220, 220));
     text.setText(string);
-    if(text.getLayoutBounds().getWidth()>150){
+    if (text.getLayoutBounds().getWidth() > 150) {
       text.setWrappingWidth(150);
     }
-    rectangle.setWidth(text.getLayoutBounds().getWidth()+20);
-    rectangle.setHeight(text.getLayoutBounds().getHeight()+20);
-    if(rectangle.getHeight()<45){
+    rectangle.setWidth(text.getLayoutBounds().getWidth() + 20);
+    rectangle.setHeight(text.getLayoutBounds().getHeight() + 20);
+    if (rectangle.getHeight() < 45) {
       rectangle.setArcHeight(25);
       rectangle.setArcWidth(25);
     } else {
       rectangle.setArcHeight(40);
       rectangle.setArcWidth(40);
     }
-    text.setLayoutY(rectangle.getHeight()/2-text.getLayoutBounds().getHeight()/2+12);
+    text.setLayoutY(rectangle.getHeight() / 2 - text.getLayoutBounds().getHeight() / 2 + 12);
     text.setLayoutX(10);
     hBox.getChildren().add(group);
-    if(isCurrentUser){
+    if (isCurrentUser) {
       rectangle.setFill(Color.rgb(0, 190, 255));
       text.setFill(Color.WHITE);
       hBox.setAlignment(Pos.TOP_RIGHT);
     }
     return hBox;
   }
+
   private void hoverButton(Node n) {
     n.setOnMouseEntered(e -> {
       n.setEffect(new Lighting());
@@ -175,6 +187,15 @@ public class MatchController implements Initializable {
     n.setOnMouseExited(e -> {
       n.setEffect(null);
     });
+  }
+
+  private void clickButton(Node n, User user1, int i) {
+    n.setOnMouseClicked(e -> {
+      chatId = i;
+      textBox.getChildren().clear();
+      fillChat(user, user1);
+    });
+
   }
 
   protected static Group createMatchCard(User user) {
@@ -210,21 +231,22 @@ public class MatchController implements Initializable {
     label.setMaxWidth(label.getPrefWidth());
     return group;
   }
+
   @FXML
-  public void animateProfile(){
+  public void animateProfile() {
     chatPic.setDisable(true);
-    if(profilePane.getPrefHeight()!=430){
-    KeyValue kv = new KeyValue(profilePane.prefHeightProperty(), 430, Interpolator.EASE_BOTH);
-    Timeline timeline = new Timeline(new KeyFrame(Duration.millis(300), kv));
-    Pane pane = SecondaryController.createCard(user);
-    profilePane.getChildren().add(pane);
-    pane.setLayoutY(70);
-    TranslateTransition tt = new TranslateTransition(Duration.millis(300), pane);
-    tt.setFromX(400);
-    tt.setToX(45);
-    SequentialTransition st = new SequentialTransition(timeline,tt);
-    st.setOnFinished(e -> chatPic.setDisable(false));
-    st.play();
+    if (profilePane.getPrefHeight() != 430) {
+      KeyValue kv = new KeyValue(profilePane.prefHeightProperty(), 430, Interpolator.EASE_BOTH);
+      Timeline timeline = new Timeline(new KeyFrame(Duration.millis(300), kv));
+      Pane pane = SecondaryController.createCard(user);
+      profilePane.getChildren().add(pane);
+      pane.setLayoutY(70);
+      TranslateTransition tt = new TranslateTransition(Duration.millis(300), pane);
+      tt.setFromX(400);
+      tt.setToX(45);
+      SequentialTransition st = new SequentialTransition(timeline, tt);
+      st.setOnFinished(e -> chatPic.setDisable(false));
+      st.play();
     } else {
       KeyValue kv = new KeyValue(profilePane.prefHeightProperty(), 62, Interpolator.EASE_BOTH);
       Timeline timeline = new Timeline(new KeyFrame(Duration.millis(300), kv));
@@ -237,7 +259,7 @@ public class MatchController implements Initializable {
         profilePane.getChildren().remove(pane);
         chatPic.setDisable(false);
       });
-      SequentialTransition st = new SequentialTransition(tt,timeline);
+      SequentialTransition st = new SequentialTransition(tt, timeline);
       st.play();
     }
   }
@@ -245,12 +267,12 @@ public class MatchController implements Initializable {
   private void fillChat(User user, User user1) {
     try {
       Chat messages = clientHandler.getChat(user, user1);
-      for(HashMap<String,String> map : messages.getMessages()){
-        if(map.containsKey(user1.getEmail())){
-          HBox hBox = createMessage(map.get(user1.getEmail()).replace("\"",""),false);
+      for (HashMap<String, String> map : messages.getMessages()) {
+        if (map.containsKey(user1.getEmail())) {
+          HBox hBox = createMessage(map.get(user1.getEmail()).replace("\"", ""), false);
           textBox.getChildren().add(hBox);
         } else {
-          HBox hBox = createMessage(map.get(user.getEmail()).replace("\"",""),true);
+          HBox hBox = createMessage(map.get(user.getEmail()).replace("\"", ""), true);
           textBox.getChildren().add(hBox);
         }
       }
