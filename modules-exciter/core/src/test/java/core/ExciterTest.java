1a92e11fa397258d9391f2e44c5ab86770b0abf3
Henrik Lovhaug <henrik.lovhaug@gmail.com>
2021-11-11 20:53:02 +0100

    Checkstyle fix

--- modules-exciter/core/src/test/java/core/ExciterTest.java
+++ modules-exciter/core/src/test/java/core/ExciterTest.java
@@ -11,63 +11,60 @@ import user.User;
 
 public class ExciterTest {
 
-    private Exciter exciter;
-    private User user = new User("test",22,"test@mail.no");
-    private BotUser botUser = new BotUser("Sofie", 23, "sofie@mail.no", true,1);
+  private Exciter exciter;
+  private User user = new User("test", 22, "test@mail.no");
+  private BotUser botUser = new BotUser("Sofie", 23, "sofie@mail.no", true, 1);
 
-    @BeforeEach
-    public void setUp() {
-        exciter = new Exciter();
-        user = new User("test",22,"test@mail.no");
-        botUser = new BotUser("Sofie", 23, "sofie@mail.no", true,2);
+  @BeforeEach
+  public void setUp() {
+    exciter = new Exciter();
+    user = new User("test", 22, "test@mail.no");
+    botUser = new BotUser("Sofie", 23, "sofie@mail.no", true, 2);
+  }
+
+  @Test
+  public void likeFirst() {
+    exciter.likePerson(user, botUser);
+    Assertions.assertTrue(user.getLikedUsers().containsKey(botUser.getEmail()));
+  }
+
+  @Test
+  public void testMatch() {
+    for (int i = 0; i < 2; i++) {
+      exciter.likePerson(user, botUser);
     }
+    Assertions.assertTrue(exciter.likePerson(user, botUser));
+    Assertions.assertEquals("sofie@mail.no", exciter.getUserMatches(user).get(0));
+  }
 
-    @Test
-    public void likeFirst() {
-        exciter.likePerson(user, botUser);
-        Assertions.assertTrue(user.getLikedUsers().containsKey(botUser.getEmail()));
-    }
+  @Test
+  public void testAddUsers() {
+    User testUser = new User("Roger", 25, "RogerNew@mail.no");
+    User testUser2 = new User("Bob", 25, "Bob@mail.no");
+    User testUser3 = new User("Rolf", 25, "Rolf@mail.no");
+    User testUser4 = new User("Bjarne", 25, "Bjarne@mail.no");
+    User testUser5 = new User("Unni", 25, "Unni@mail.no");
+    User testUser6 = new User("Roger", 25, "RogerNew@mail.no"); // duplicate should not be added
 
-    @Test
-    public void testMatch() {
-        for (int i = 0; i < 2; i++) {
-            exciter.likePerson(user, botUser);
-        }
-        Assertions.assertTrue(exciter.likePerson(user,botUser));
-        Assertions.assertEquals("sofie@mail.no", exciter.getUserMatches(user).get(0));
-    }
+    List<User> testUsers = List.of(testUser, testUser2, testUser3, testUser4, testUser5, testUser6);
+    int count = exciter.getAllUsers().size();
+    exciter.addUsers(testUsers);
+    Assertions.assertEquals(count + 5, exciter.getAllUsers().size());
+  }
 
+  @Test
+  public void testGetUserByEmail() {
+    exciter.addUser(user);
+    Assertions.assertEquals(user, exciter.getUserByEmail(user.getEmail()));
+  }
 
-    @Test
-    public void testAddUsers() {
-        User testUser = new User("Roger", 25, "RogerNew@mail.no");
-        User testUser2 = new User("Bob", 25, "Bob@mail.no");
-        User testUser3 = new User("Rolf", 25, "Rolf@mail.no");
-        User testUser4 = new User("Bjarne", 25, "Bjarne@mail.no");
-        User testUser5 = new User("Unni", 25, "Unni@mail.no");
-        User testUser6 = new User("Roger", 25, "RogerNew@mail.no"); // duplicate should not be added
+  @Test
+  public void testClearUser() {
+    exciter.addUser(user);
+    Assertions.assertTrue(exciter.getAllUsers().contains(user));
+    exciter.clearUser(user);
+    Assertions.assertFalse(exciter.getAllUsers().contains(user));
 
-        List<User> testUsers = List.of(testUser, testUser2, testUser3, testUser4, testUser5, testUser6);
-        int count = exciter.getAllUsers().size();
-        exciter.addUsers(testUsers);
-        Assertions.assertEquals(count + 5, exciter.getAllUsers().size());
-    }
-
-    @Test
-    public void testGetUserByEmail(){
-        exciter.addUser(user);
-        Assertions.assertEquals(user, exciter.getUserByEmail(user.getEmail()));
-    }
-
-    @Test
-    public void testClearUser(){
-        exciter.addUser(user);
-        Assertions.assertTrue(exciter.getAllUsers().contains(user));
-        exciter.clearUser(user);
-        Assertions.assertFalse(exciter.getAllUsers().contains(user));
-
-    }
-
-
+  }
 
 }
