a46561577b4a8ba1b06d16c25f30b740486e7e05
Henrik <henrik@stud.ntnu.no>
2021-11-11 14:32:42 +0100

    Ui now executable

--- modules-exciter/ui/src/main/java/module-info.java
+++ modules-exciter/ui/src/main/java/module-info.java
@@ -3,6 +3,9 @@ module exciter.ui {
     requires transitive exciter.user;
     requires transitive javafx.controls;
     requires transitive javafx.fxml;
+    requires jai.imageio.core;
+    requires javafx.swing;
+    requires java.desktop;
     requires java.rmi;
     requires org.apache.commons.io;
     requires com.fasterxml.jackson.databind;
